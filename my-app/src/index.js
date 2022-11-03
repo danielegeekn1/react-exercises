d73073cf00085681990b326a27d295a25695b443
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-03 09:33:15 +0100

    add components-04 ex done

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from "react-dom";
+import { App } from "./Components-03";
+
+//Create a new `index.js` file within the `src` folder and render the `App` component using the `ReactDOM.render` method.
+const root = document.querySelector("#root");
+
+ReactDom.render(<App />, root);
