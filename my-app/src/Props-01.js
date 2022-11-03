28f949c5789958c27a9db4a17aae03aec54c05b7
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-03 11:06:06 +0100

    add props-03 ex done

--- my-app/src/Props-01.js
+++ my-app/src/Props-01.js
@@ -2,6 +2,12 @@
 import React from "react";
 export class Welcome extends React.Component {
   render() {
-    return <p>Welcome {this.props.name}</p>;
+    return (
+      <div>
+        <p>Welcome {this.props.name}</p>
+        <p>Your age is {this.props.age}</p>
+      </div>
+    );
   }
 }
+//Modify the `Welcome` component so that it receives a second prop called `age` and renders it below the 'welcome' message, within a `p` tag after the message `"Your age is "`.
