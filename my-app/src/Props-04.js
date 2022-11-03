880f24baa43ca423e76f41805b724a86789ee33d
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-03 12:15:41 +0100

    ex props-04done

---
+++ my-app/src/Props-04.js
@@ -0,0 +1,3 @@
+//What happens if the `name` prop is a JSX expression instead of a string? How do you pass a prop that contains a JSX expression? Modify the value passed to the `name` prop so that it's contained within a `strong` tag.
+//you can add a JS expression in JSX as props, in order to do it it's necessary to put the expressione in curley brackets as in this example below
+//foo={1 + 2 + 3 + 4}
