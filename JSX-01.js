b5a34a2e809f785e0b4105d2754d101144d1e99a
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-03 08:47:05 +0100

    JSX-01

--- JSX-01.js
+++ JSX-01.js
@@ -4,7 +4,7 @@ Create a variable called `hello` and inizialize it with an `h1` tag with the mes
 import React from "react";
 
 const hello = "world";
-export class Hello extends React.Component {
+export class HelloWorld extends React.Component {
   render() {
     return <h1>Hello {hello}</h1>;
   }
