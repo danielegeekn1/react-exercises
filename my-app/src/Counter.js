95d6c59169e68414eb58c804302450f1f371ab0e
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-03 13:23:28 +0100

    state-01 ex done

---
+++ my-app/src/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+export class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + 1,
+        };
+      });
+    }, 1000);
+  }
+  render() {
+    return <h1>{this.state.count}</h1>;
+  }
+}
+//State 01
+
+//Create a `Counter` class component with an internal state containing a `count` property, initialized to `0`. The `Counter` component should render the `count` property within an `h1` tag, and the `count` property should be incremented by `1` every second;
