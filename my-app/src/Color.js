8e95db3a500eec42e05578e1867003e061c95175
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-04 11:41:57 +0100

    add lists-02 ex done

--- my-app/src/Color.js
+++ my-app/src/Color.js
@@ -7,8 +7,8 @@ export class Color extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.items.map((color, index) => (
-          <li key={color + index}>{color}</li>
+        {this.props.items.map((color) => (
+          <li key={color.id}>{color.name}</li>
         ))}
       </ul>
     );
@@ -16,3 +16,7 @@ export class Color extends React.Component {
 }
 //in order to make it work I wrote the items array inside app component
 //i passed mycolor component in myapp component ( in components-03), and there i passed my items array whch i cycled in this component here
+
+//Lists 02
+
+//Modify the `Colors` component so that `items` is expected to be an array of objects each containing an `id` and a `name` property. Render the `name` property of each object within a `li` tag, and use the `id` as a key.
--- my-app/src/Components-03.js
+++ my-app/src/Components-03.js
@@ -3,7 +3,22 @@ import React from "react";
 import { Color } from "./Color";
 import { Hello } from "./Component-02";
 import { Welcome } from "./Props-01";
-const items = ["red", "white", "red", "black"];
+const items = [
+  { id: 1, name: "green" },
+  {
+    id: 2,
+    name: "red",
+  },
+  {
+    id: 3,
+    name: "white",
+  },
+  {
+    id: 4,
+    name: "blue",
+  },
+];
+
 export class App extends React.Component {
   render() {
     return (
