388c1cdd5dd95e9a5219d5b6081183e34c0d3ea4
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-06 11:28:56 +0100

    add context-01 ex done

--- my-app/src/Components-03.js
+++ my-app/src/Components-03.js
@@ -4,6 +4,7 @@ import { ClickTracker } from "./ClickTracker";
 import { Color } from "./Color";
 import { Hello } from "./Component-02";
 import { Container } from "./Container";
+import { DisplayLanguage } from "./LanguageContext";
 import { Welcome } from "./Props-01";
 import { TodoList } from "./TodoList";
 const items = [
@@ -24,17 +25,34 @@ const items = [
 
 const ItemsF = ["billy", "kate", "jane"];
 export class App extends React.Component {
+  state = {
+    language: "en",
+  };
+  handleLanguageChange = (e) => {
+    this.setState({
+      language: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <Hello />
-        <Welcome name="Daniele" age={33} />
-        <Color items={items} />
-        <ClickTracker />
-        <TodoList items={ItemsF} />
+        <select
+          value={this.state.language}
+          onChange={this.handleLanguageChange}
+        >
+          <option value="en">ENGLISH</option>
+          <option value="it">ITALIAN</option>
+        </select>
+        <DisplayLanguage.Provider>
+          <Hello />
+          <Welcome name="Daniele" age={33} />
+          <Color items={items} />
+          <ClickTracker />
+          <TodoList items={ItemsF} />
 
-        <Container>
-          {/*  {(position) => {
+          <Container>
+            {/*  {(position) => {
             const [x, y] = position;
             return (
               <div>
@@ -42,15 +60,16 @@ export class App extends React.Component {
               </div>
             );
           }}*/}
-          {(title) => {
-            const [title1, title2] = title;
-            return (
-              <div>
-                The first title is {title1}, while the second one is{title2}
-              </div>
-            );
-          }}
-        </Container>
+            {(title) => {
+              const [title1, title2] = title;
+              return (
+                <div>
+                  The first title is {title1}, while the second one is{title2}
+                </div>
+              );
+            }}
+          </Container>
+        </DisplayLanguage.Provider>
       </div>
     );
   }
---
+++ my-app/src/LanguageContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+export const DisplayLanguage = createContext("en");
+
+//Create a LanguageContext and wrap a DisplayLanguage component within its provider
+// Add a select tag to the component containing the DisplayLanguage component
+//that allows the user to select the language, and pass it as a value to the Provider.
