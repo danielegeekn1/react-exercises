83ce477c1d20642f00e8f7ba8fe7a11bd5605f88
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-06 11:00:20 +0100

    begin ex

--- my-app/src/Components-03.js
+++ my-app/src/Components-03.js
@@ -32,6 +32,7 @@ export class App extends React.Component {
         <Color items={items} />
         <ClickTracker />
         <TodoList items={ItemsF} />
+
         <Container>
           {/*  {(position) => {
             const [x, y] = position;
--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -6,22 +6,22 @@ export class TodoList extends React.Component {
     value: "",
   };
   addInputValue = (e) => {
-    const values = e.target.value;
+    // const values = e.target.value;
     this.setState({
       //value: this.state.value + values,
       //ItemsF.push(value)
-      value: ItemsF.push(values),
+      //value: ItemsF.push(values),
+      value: ItemsF,
     });
   };
 
   render() {
     return (
       <div>
-        <ul>
-          {this.props.items.map((item, index) => (
-            <li key={item + index}>{item}</li>
-          ))}
-        </ul>
+        {this.props.items.map((item, index) => (
+          <li key={item + index}>{item}</li>
+        ))}
+
         <input type="username" value={this.state.username} />
         <button onClick={this.addInputValue}></button>
         <button onClick={this.resetBtn}></button>
@@ -31,7 +31,9 @@ export class TodoList extends React.Component {
 }
 
 //Lists 03
+/*
 
+*/
 //Create a `TodoList` component that renders a `ul` tag with a `li` tag for each item contained in the `items` _state variable_.
 // The `items` state variable should be an array of strings.
 //The `TodoList` component should also contain an `input` tag and a `button`.
@@ -47,3 +49,10 @@ export class TodoList extends React.Component {
 //render-props-02
 //Change the `TodoList` component so that instead of using the `render` prop it uses the `children` prop.
 //How does this affect the component in which the `TodoList` component is rendered?
+
+//render props-01
+
+//Modify the `TodoList` component so that, instead of rendering the `items` array within the `ul` tag,
+// it calls the function passed to its `render` prop,
+// passing it the `items` array as a parameter, as well as the function required to delete the item.
+// Pass a render prop to the `TodoList` component to correctly render and interact with the `items` array.
