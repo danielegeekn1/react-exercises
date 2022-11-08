fec81e1f1736b358f54efc1dfdf414b6fe51d568
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-08 14:52:32 +0100

    first commit

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -2,6 +2,22 @@
 import React from "react";
 export class Age extends React.Component {
   render() {
-    return <p>Your age is {this.props.age}</p>;
+    return (
+      <div>
+        {this.props.age > 18 ? (
+          <p>Your age is {this.props.age}</p>
+        ) : (
+          <p>Your too young</p>
+        )}
+      </div>
+    );
   }
 }
+//conditional rendering 01
+//Modify the Welcome(err age) component so that the Age component
+//is rendered only if the age prop is greater than 18.
+
+//conditional rendering 02
+//Modify the Welcome(err age) component so that the Age component is rendered only if the age prop is present.
+
+//
