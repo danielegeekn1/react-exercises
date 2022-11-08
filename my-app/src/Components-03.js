32fbbb71489af5a5f01097470a7d76662dbcc496
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-08 15:02:13 +0100

    change, ex cond-rendering-4 really correct

--- my-app/src/Components-03.js
+++ my-app/src/Components-03.js
@@ -1,5 +1,6 @@
 //Write a class component called `App` that renders the `Hello` component within a `div` tag.
 import React from "react";
+import { Age } from "./Age";
 import { Hello } from "./Component-02";
 import { Welcome } from "./Props-01";
 export class App extends React.Component {
@@ -8,6 +9,7 @@ export class App extends React.Component {
       <div>
         <Hello />
         <Welcome name="Daniele" age={33} />
+        <Age name="john" age={33} />
       </div>
     );
   }
