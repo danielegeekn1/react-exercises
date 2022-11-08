6d4548b0547959097c164e3d08241c21e1bac0dc
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-08 14:51:35 +0100

    change

--- my-app/src/Props-01.js
+++ my-app/src/Props-01.js
@@ -1,16 +1,12 @@
 //Create a `Welcome` class component that receives a `name` prop and renders the `Welcome, {name}!` message within a `p` tag. Render this component to you `App` component, passing it a `name` prop of your choosing.
 import React from "react";
-import { Age } from "./Age";
+
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
         <p>Welcome {this.props.name}</p>
         <p>Your age is {this.props.age}</p>
-        {Age.props.age > 18 && <Age age={33} />}
-        {Age.props.age && <Age />}
-        {Age.props.age > 18 && Age.props.age < 65 && <Age />}
-        {Age.props.age > 18 && Age.props.age < 65 && Age.props.name && <Age />}
       </div>
     );
   }
@@ -23,11 +19,3 @@ export class Welcome extends React.Component {
 //Conditional Rendering 02
 
 //Modify the `Welcome` component so that the `Age` component is rendered only if the `age` prop is present.
-
-//Conditional Rendering 03
-
-//Modify the `Welcome` component so that the `Age` component is rendered only if the `age` prop is greater than `18` and less than `65`.
-
-//Conditional Rendering 04
-
-//Modify the `Welcome` component so that the `Age` component is rendered only if the `age` prop is greater than `18` and less than `65` and the `name` prop is equal to "John".
