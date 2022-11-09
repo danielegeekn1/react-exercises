8484b898d04535df4a595a2fd466b2e25e9ea475
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-09 16:40:40 +0100

    add useCallback ex done

--- my-app/src/Components-03.js
+++ my-app/src/Components-03.js
@@ -5,7 +5,7 @@ import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Color } from "./Color";
 import { Hello } from "./Component-02";
-import { Container } from "./Container";
+
 import { ControlInputs } from "./ControlInputs";
 
 import { Counter } from "./Counter";
@@ -142,27 +142,6 @@ export function App() {
         <ClickTracker />
         <TodoList items={ItemsF} />
         <ClickCounter initialValue={3} incrementsBy={1} />
-        <Container>
-          {(position) => {
-            const [x, y] = position;
-            return (
-              <div>
-                The current position is{x},{y}
-              </div>
-            );
-          }}
-          {(title) => {
-            title = ["My Title", "My Title 2"];
-            const [title1, title2] = title;
-            return (
-              <div>
-                <h1>
-                  The first title is {title1}, while the second one is{title2}
-                </h1>
-              </div>
-            );
-          }}
-        </Container>
       </DisplayLanguage.Provider>
     </div>
   );
--- my-app/src/UseCounter.js
+++ my-app/src/UseCounter.js
@@ -1,7 +1,7 @@
 //ex custom hooks-01
 //Create a custom hook `useCounter` that keeps track of the state of a counter, and returns the current value of the counter
 // as well as three functions to increment, decrement and reset the counter.
-import { useState } from "react";
+import { useCallback, useState } from "react";
 export function useCounter(initialValue = 0, incrementor = 1) {
   const [count, setCount] = useState(initialValue);
   /*
@@ -19,15 +19,17 @@ export function useCounter(initialValue = 0, incrementor = 1) {
     };
   }, [initialValue, incrementor, timeout]);
   */
-  function incrementCounter() {
+
+  const incrementCounter = useCallback(() => {
     setCount((c) => c + incrementor);
-  }
-  function decrementCounter() {
-    setCount((c) => c - 1);
-  }
-  function resetCounter() {
+  }, [incrementor]);
+
+  const decrementCounter = useCallback(() => {
+    setCount((c) => c - incrementor);
+  }, [incrementor]);
+  const resetCounter = useCallback(() => {
     setCount(initialValue);
-  }
+  }, [initialValue]);
   return {
     count,
     onIncrement: incrementCounter,
@@ -35,3 +37,6 @@ export function useCounter(initialValue = 0, incrementor = 1) {
     onReset: resetCounter,
   };
 }
+//ex useCallback
+//Modify the useCounter custom hook from Custom Hooks 01
+// to use the useCallback to memoize the functions used to increment, decrement and reset the counter
