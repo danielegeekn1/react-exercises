efd19c88318550c4d8dc85871139ef5da819b297
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-14 11:34:06 +0100

    changes - corrected ex

--- my-app/src/UncontrolledLogin.js
+++ my-app/src/UncontrolledLogin.js
@@ -1,10 +1,18 @@
-import React, { createRef } from "react";
+import React from "react";
+import { createRef } from "react";
 
 export class UncontrolledLogin extends React.Component {
   _formRef = createRef();
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const remember = e.target.elements.remember.checked;
+    console.log({ username, password, remember });
+  };
   render() {
     return (
-      <form ref={this._formRef}>
+      <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
         <input type="username" />
         <input type="password" />
         <input type="checkbox" />
