25169091eae306a41cc547d82ea4be1b67217c00
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-22 18:48:53 +0100

    correction

--- my-app/src/Props-01.js
+++ my-app/src/Props-01.js
@@ -4,7 +4,7 @@ export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <p>Welcome {this.props.name}</p>
+        <strong>Welcome {this.props.name}</strong>
         <p>Your age is {this.props.age}</p>
       </div>
     );
