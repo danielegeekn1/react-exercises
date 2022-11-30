13e22041605b17a2d8d209e3fb2397da4e4a9a01
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-11-30 09:55:07 +0100

    add first part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import data from "./Components/Back/Data/Data";
+import Header from "./Components/Front/Header/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+const App = () => {
+  const { productItems } = data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Back/Data/Data.js
@@ -0,0 +1,59 @@
+const data = {
+  productItems: [
+    {
+      id: 1,
+      name: "Beats Headphone",
+      price: 99.99,
+      image: "./Pics/headphones.png",
+    },
+    {
+      id: 2,
+      name: "Sol Headphone",
+      price: 89.99,
+      image: "./Pics/headphones.png",
+    },
+    {
+      id: 3,
+      name: "iphone x",
+      price: 1099.99,
+      image: "./Pics/iphonex.jpeg",
+    },
+    {
+      id: 4,
+      name: "Samsung galaxy Tab s7",
+      price: 599.99,
+      image: "./Pics/samsung-galaxy-tab7.jpeg",
+    },
+    {
+      id: 5,
+      name: "Samsung Galaxy Tab s8",
+      price: 999.99,
+      image: "./Pics/samsung-galaxy-tab8.jpg",
+    },
+    {
+      id: 6,
+      name: "Asus gaming pc",
+      price: 1999.99,
+      image: "./Pics/asus-gaming-pc.jpeg",
+    },
+    {
+      id: 7,
+      name: "MacBook Air M1",
+      price: 2299.99,
+      image: "./Pics/macbook-air-m1.jpeg",
+    },
+    {
+      id: 8,
+      name: "SmartWatch",
+      price: 2299.99,
+      image: "./Pics/smartwatch.jpeg",
+    },
+    {
+      id: 9,
+      name: "Woman Smartwatch",
+      price: 799.99,
+      image: "./Pics/smartwatch-pink.jpg",
+    },
+  ],
+};
+export default data;
